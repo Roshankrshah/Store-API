13678616be12ce6224d996636287c5b8051dcb14
Roshan <shahroshan0720@gmail.com>
2023-06-14 11:24:07 +0530

    setup the conection to DB

--- app.js
+++ app.js
@@ -5,5 +5,27 @@ const app = express();
 
 const errorMiddleware = require('./middleware/error-handler');
 const notfoundMiddleware = require('./middleware/not-found');
+const connectDB = require('./db/connect');
 
 app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('<h1>store API</h1><a href="/api/v1/products">products route</a>');
+});
+
+app.use(errorMiddleware);
+app.use(notfoundMiddleware);
+
+const port = process.env.PORT || 3000
+
+const start = async ()=>{
+    try {
+        //connect DB
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port,console.log(`Server is listen at http://localhost:${port}`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
